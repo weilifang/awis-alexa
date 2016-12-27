81b0f75d85eb5cfb931190b6b2f5f543350d56c3
weijihao <1005474666@qq.com>
2016-12-27 18:01:38 +0800

    Update file

--- src/alexa.php
+++ src/alexa.php
@@ -8,14 +8,14 @@ class Alexa
 	 * @var string
 	 */
     protected $accessKeyId;
-	
+
 	/**
 	 * 私有访问密钥
 	 *
 	 * @var string
 	 */
     protected $secretAccessKey;
-	
+
 	/**
 	 * 请求API的方法名称
 	 *
@@ -24,17 +24,17 @@ class Alexa
 	protected static $ActionName = 'UrlInfo';
 
     protected $httpClient;
-	
-	
+
+
 	/**
 	 * 服务地址
 	 *
 	 * @var int
 	 */
 	protected $ServiceHost = 'awis.amazonaws.com';
-	
+
 	protected $HashAlgorithm = 'HmacSHA256';
-	
+
 	/**
 	 * 版本选择
 	 *
@@ -53,30 +53,37 @@ class Alexa
         $this->accessKeyId = $accessKeyId;
         $this->secretAccessKey = $secretAccessKey;
     }
-	
+
 	public function getUrlInfo($site, $responseGroup = "ContentData")
 	{
-		//$this->site = $site;
-		
+		//传入参数,创建出参数数组
         $queryParams = $this->buildQueryParams($site, $responseGroup);
-		
-		
+
+
+        //加入签名
         $sig = $this->generateSignature($queryParams);
-		
-		
-		
-        $url = 'http://' . $this->ServiceHost . '/?' . $queryParams . 
-            '&Signature=' . $sig;
-		
-		
+
+
+
+        $url = 'http://' . $this->ServiceHost . '/?' . $queryParams . '&Signature=' . $sig;
+
+
         $ret = self::makeRequest($url);
+
 		var_dump($ret);
-		
-		
+
+
         echo "\nResults for " . $site .":\n\n";
-		
+
+
+        //这里经常报错
         self::parseResponse($ret);
 	}
+
+    protected function buildQuery($params)
+    {
+        return http_build_query($params);
+    }
 
     /**
      * Builds query parameters for the request to AWIS.
@@ -84,19 +91,19 @@ class Alexa
 	 *
      * Parameter names will be in alphabetical order and
      * parameter values will be urlencoded per RFC 3986.
-     * @return String query parameters for the request 
+     * @return String query parameters for the request
      */
     protected function buildQueryParams($site, $responseGroup)
     {
         $params = array(
-            'Action'            => self::$ActionName,//接口方法 
+            'Action'            => self::$ActionName,//接口方法
             'ResponseGroup'     => $responseGroup,
             'AWSAccessKeyId'    => $this->accessKeyId,//访问密钥 ID
             'Timestamp'         => self::getTimestamp(),//得到时间戳
             //'Count'             => self::$NumReturn,
             //'Start'             => self::$StartNum,
-            'SignatureVersion'  => $this->SigVersion,
-            'SignatureMethod'   => $this->HashAlgorithm,
+            'SignatureVersion'  => $this->SigVersion,//签名版本
+            'SignatureMethod'   => $this->HashAlgorithm,//签名方法
             'Url'               => $site,
         );
         ksort($params);//数组排序
@@ -106,20 +113,22 @@ class Alexa
         }
         return implode('&', $keyvalue);
     }
-	
+
     /**
      * Parses XML response from AWIS and displays selected data
      * @param String $response    xml response from AWIS
      */
     public static function parseResponse($response) {
         $xml = new SimpleXMLElement($response, null, false, 'http://awis.amazonaws.com/doc/2005-07-11');
+
         if ($xml->count() && $xml->Response->UrlInfoResult->Alexa->count()) {
             $info = $xml->Response->UrlInfoResult->Alexa;
             $nice_array = array(
                 'Links In Count' => $info->ContentData->LinksInCount,
-                'Rank'           => $info->TrafficData->Rank
+                'Rank'           => $info->TrafficData->Rank,
             );
         }
+
         foreach ($nice_array as $k => $v) {
             echo $k . ': ' . $v ."\n";
         }
@@ -130,19 +139,19 @@ class Alexa
      *
      * @param String $url       URL to use in createing signature
      */
-    protected function generateSignature($url) 
+    protected function generateSignature($url)
 	{
         $sign = "GET\n" . strtolower($this->ServiceHost) . "\n/\n" . $url;
-		
-		
+
+
         echo "String to sign: \n" . $sign . "\n";
-		
-		
+
+
         $sig = base64_encode(hash_hmac('sha256', $sign, $this->secretAccessKey, true));
-		
-		
-        echo "\nSignature: " . $sig ."\n";
-		
+
+
+        echo "\n 签名: " . $sig ."\n";
+
         return rawurlencode($sig);//rawurlencode — 按照 RFC 3986 对 URL 进行编码
     }
 
@@ -151,21 +160,22 @@ class Alexa
      * @param String $url   URL to make request to
      * @return String       Result of request
      */
-    protected static function makeRequest($url) 
+    protected static function makeRequest($url)
 	{
-        echo "\nMaking request to:\n$url\n";
+        //echo "\nMaking request to:\n$url\n";
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_TIMEOUT, 4);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $result = curl_exec($ch);
         curl_close($ch);
+
         return $result;
     }
 
     /**
      * Builds current ISO8601 timestamp.
 	 * [构建当前ISO8601时间戳]
-	 *
+	 * @return string
      */
     protected static function getTimestamp()
     {
--- test.php
+++ test.php
@@ -3,4 +3,4 @@ require_once 'src/alexa.php';
 
 $awis = new Alexa('AKIAJBV37W6XMKGUTKPA', '+W3OmPXZ6WNydEK2TbRVYRE7pqc57/Lhex72iCmP');
 
-$awis->getUrlInfo('http://www.baidu.com', 'Rank,LinksInCount');
+$awis->getUrlInfo('http://www.baidu.com', 'Rank,LinksInCount,RankByCountry,UsageStats,AdultContent');//Rank,LinksInCount
